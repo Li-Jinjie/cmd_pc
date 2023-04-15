f9656c35f77e02639626af05dc3c54862c69d37c
Jinjie Li <lijinjie362@outlook.com>
2023-04-15 17:59:07 +0800

    finish TrackTraj.action setup, tested successfully!

--- scripts/planner_node.py
+++ scripts/planner_node.py
@@ -8,6 +8,7 @@ Description:
 """
 import sys
 import os
+import time
 
 current_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, current_path)
@@ -15,6 +16,9 @@ sys.path.insert(0, current_path)
 import rospy
 from transitions import Machine
 import random
+import actionlib
+
+from oop_qd_onbd.msg import TrackTrajAction, TrackTrajGoal, TrackTrajResult, TrackTrajFeedback
 
 
 class PlannerNode(object):
@@ -23,36 +27,61 @@ class PlannerNode(object):
     states = ["GROUND", "TAKEOFF", "PLANNING", "EXECUTION", "LAND"]
     # states = ["asleep", "hanging out", "hungry", "sweaty", "saving the world"]
 
-    def __init__(self, name):
-        # No anonymous superheroes on my watch! Every narcoleptic superhero gets
-        # a name. Any name at all. SleepyMan. SlumberGirl. You get the idea.
-        self.name = name
+    def __init__(self):
+        rospy.init_node("planner_node", anonymous=False)
 
-        # What have we accomplished today?
-        self.kittens_rescued = 0
+        # Action Client
+        self.tracking_client = actionlib.SimpleActionClient("tracking_controller/track_traj", TrackTrajAction)
+        self.tracking_client.wait_for_server()
+        rospy.loginfo("Action Service exist: tracking_controller/track_traj")
 
-        # Initialize the state machine
-        self.machine = Machine(model=self, states=PlannerNode.states, initial="GROUND")
+        # Finite State machine
+        self.machine = Machine(model=self, states=PlannerNode.states, initial="PLANNING")
 
-        # Add some transitions. We could also define these using a static list of
-        # dictionaries, as we did with states above, and then pass the list to
-        # the Machine initializer as the transitions= argument.
-
-        # At some point, every superhero must rise and shine.
-        self.machine.add_transition(trigger="take_off", source="GROUND", dest="EXECUTION", after="pub_takeoff_traj")
-        self.machine.add_transition(trigger="finish_planning", source="PLANNING", dest="EXECUTION", after="pub_traj")
+        # self.machine.add_transition(trigger="take_off", source="GROUND", dest="EXECUTION", after="set_takeoff_traj")
+        self.machine.add_transition(trigger="finish_planning", source="PLANNING", dest="EXECUTION", after="set_traj")
         self.machine.add_transition(trigger="reach_target", source="EXECUTION", dest="PLANNING", after="planning_traj")
-        self.machine.add_transition(trigger="land", source="*", dest="LAND", after="pub_landing")
-        self.machine.add_transition(trigger="touch_ground", source="LAND", dest="GROUND")
+        # self.machine.add_transition(trigger="land", source="*", dest="LAND", after="set_landing")
+        # self.machine.add_transition(trigger="touch_ground", source="LAND", dest="GROUND")
 
-    def pub_takeoff_traj(self):
-        pass
-
-    def pub_traj(self):
-        pass
+        # start
+        self.goal = TrackTrajGoal()
+        self.planning_traj()
 
     def planning_traj(self):
-        pass
+        rospy.loginfo("Start planning trajectory......")
 
-    def pub_landing(self):
+        # TODO: planning trajectory
+        time.sleep(1)
+        self.goal = TrackTrajGoal()
+
+        rospy.loginfo("Finish planning trajectory.")
+        self.finish_planning()  # trigger the FMS to change state
+
+    def set_traj(self):
+        self.tracking_client.send_goal(self.goal, feedback_cb=self.track_traj_feedback_cb)
+
+        self.tracking_client.wait_for_result()
+
+        result: TrackTrajResult = self.tracking_client.get_result()
+
+        rospy.loginfo("Reach target.")
+        rospy.loginfo(f"Trajectory tracking RMSE: {result.tracking_error_rmse} m")
+        self.reach_target()  # trigger the FMS to change state
+
+    def track_traj_feedback_cb(self, feedback: TrackTrajFeedback):
+        rospy.loginfo(f"Trajectory tracking percent complete: {feedback.percent_complete}")
+
+    # def set_takeoff_traj(self):
+    #     pass
+    #
+    # def set_landing(self):
+    #     pass
+
+
+if __name__ == "__main__":
+    try:
+        node = PlannerNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
         pass
